cbfb2addeb0c175701e744904d025e8d0a9bf4c1
Pierre Thierry <pierre@nothos.net>
2017-01-14 11:26:42 +0100

    Test timer start/stop

--- test/jedossi_test.exs
+++ test/jedossi_test.exs
@@ -7,4 +7,27 @@ defmodule JedossiTest do
 	Jedossi.store_value(:foo, :bar)
 	assert :bar == Jedossi.get_value(:foo)
   end
+
+  test "start" do
+	Jedossi.create_store()
+	Jedossi.start_timer(:foo)
+
+	old = Jedossi.get_value(:foo)
+	assert is_list(old)
+
+	Jedossi.start_timer(:foo)
+	assert old == Jedossi.get_value(:foo)
+  end
+
+  test "stop" do
+	Jedossi.create_store()
+
+	Jedossi.stop_timer(:foo)
+	assert [] == Jedossi.get_value(:foo)
+
+	Jedossi.start_timer(:foo)
+	Jedossi.stop_timer(:foo)
+
+	assert 1 == length(Jedossi.get_value(:foo))
+  end
 end
